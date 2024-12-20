4f630d0912f3fbe370043f4b2ca0324c6c7b9f18
k3rlyn <kerlyndesliaa@gmail.com>
2024-12-20 21:19:24 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://thriftify.vercel.app/'],
     credentials: true
 }));
 
